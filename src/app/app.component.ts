4c1b616b59edfd340e6f020a6d6dba1cae4b0ee5
juanirimoldi <69910713+juanirimoldi@users.noreply.github.com>
2022-03-07 19:19:48 -0300

    agregando typescript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,59 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lista-tareas';
+  // Agrego una nueva tarea a la lista de tareas.
+function createTask() {
+  var li = document.createElement("li");
+  
+  var texto = document.getElementById("text").value;
+  var task = document.createElement("label");
+  task.setAttribute("class","container");
+  var t = document.createTextNode(texto);
+  task.appendChild(t);
+  
+  var checkb = document.createElement("input");
+  checkb.setAttribute("type","checkbox");
+  checkb.addEventListener('click', completeTask);
+
+  var checkm = document.createElement("span");
+  checkm.setAttribute("class","checkmark");
+  
+  task.appendChild(checkb);
+  task.appendChild(checkm);
+  
+  var del = document.createElement("input");
+  del.setAttribute("type", "image");
+  del.setAttribute("src", "https://freesvg.org/img/trash.png");
+  del.setAttribute("class", "deleteIcon");
+  del.addEventListener('click', deleteTask);
+
+  li.appendChild(del);
+  li.appendChild(task);
+
+  
+ if (texto == '') {
+    alert("Debes escribir una tarea!");
+  } else {
+    document.getElementById("list").insertBefore(li, list.children[0]);
+  }
+  
+  document.getElementById("text").value = "";
+}
+
+// Marca la tarea como completada, clickeando la casilla y tachando la tarea.
+function completeTask(event){
+  var task = event.target.parentElement.parentElement;
+  if (task.className == "completed") {
+    task.removeAttribute("class");
+  } else{
+    task.setAttribute("class","completed");
+  }
+}
+
+// Elimina una tarea de la lista de tareas.
+function deleteTask(event){
+  var task = event.target.parentElement;
+  var list = task.parentElement;
+  list.removeChild(task);  
+}
 }
